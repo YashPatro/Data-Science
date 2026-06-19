e69709615cf4ab73742bddb85317259bfd42f7a7
YashPatro <rashmita@247commerce.co.uk>
2026-06-19 18:57:18 +0100

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -59,3 +59,47 @@ print(data.groupby('Pclass')['Pclass'].count())
 print(data.groupby('Sex')['Pclass'].count())
 
 print(data.sort_values(by = ['Pclass','Age'],ascending=False))
+
+#hw
+ 
+t = pd.read_csv("newTitanic.csv")
+
+t["NameLowercase"] = t["Name"].str.lower()
+t["Surname"] = t["Name"].str.split(",").str.get(0)
+t["Sex_short"] = t["Sex"].replace({"male": "M", "female": "F"})
+
+print("Mean Fare by Sex and Pclass")
+print(t.groupby(["Sex", "Pclass"])["Fare"].mean())
+
+print("Mean Age by Sex")
+print(t.groupby("Sex")["Age"].mean())
+
+print("Median Age by Sex")
+print(t.groupby("Sex")["Age"].median())
+
+print("Passengers Age 20 to 40")
+print(t[(t["Age"] >= 20) & (t["Age"] <= 40)][["Name", "Fare"]])
+
+print("Fare Greater Than 100")
+print(t[t["Fare"] > 100][["PassengerId", "Name", "Pclass"]])
+
+print("Survived and 3rd Class")
+print(t[(t["Survived"] == 1) & (t["Pclass"] == 3)]["Name"])
+
+t.loc[0:4, "Name"] = "Test Passenger"
+
+t.loc[10:15, "Fare"] = 999
+
+print("Modified Rows")
+print(t.loc[0:15, ["Name", "Fare"]])
+
+t["FarePerPerson"] = t["Fare"] / (t["SibSp"] + 1)
+
+t["AgeGroup"] = "Adult"
+t.loc[t["Age"] < 12, "AgeGroup"] = "Child"
+t.loc[(t["Age"] >= 12) & (t["Age"] <= 18), "AgeGroup"] = "Teen"
+
+print("Age Group Counts")
+print(t["AgeGroup"].value_counts())
+
+print(t[["Name", "FarePerPerson", "AgeGroup"]].head())
