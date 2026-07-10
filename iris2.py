5fa2472d1f8b7896d42803f11466528cdc86f760
YashPatro <rashmita@247commerce.co.uk>
2026-07-10 19:00:38 +0100

    Add files via upload

---
+++ iris2.py
@@ -0,0 +1,32 @@
+import pandas as pd , numpy as np, matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn import metrics 
+
+data = pd.read_csv('iris.csv')
+print(data.head())
+print()
+print(data.info())
+
+data['species'] = data['species'].replace({'setosa':0,'versicolor':1,'virginica':2})
+
+plt.subplot(221)
+plt.scatter(data['sepal_length'],data['species'],s = 10, c = 'green',marker = 'o')
+
+plt.subplot(222)
+plt.scatter(data['sepal_width'],data['species'],s = 10, c = 'red',marker = 'd')
+
+plt.subplot(223)
+plt.scatter(data['petal_length'],data['species'],s = 10, c = 'blue',marker = '^')
+plt.subplot(224)
+plt.scatter(data['petal_width'],data['species'],s = 10, c = 'cyan',marker = 'o')
+
+plt.show()
+print(data.info)
+Y = data['species']
+X = data.drop('species',axis = 1)
+Xtrain,Ytrain,Xtest,Ytest = train_test_split(X,Y,test_size = 0.3,random_state=1)
+print(Xtrain.shape)
+print(Xtrain)
