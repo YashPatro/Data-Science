326abbee17014250f18108ad8e4269d86d709062
YashPatro <rashmita@247commerce.co.uk>
2026-09-18 18:57:21 +0100

    Add files via upload

--- adults.py
+++ adults.py
@@ -24,3 +24,32 @@ for i in salary_data.columns:
 
 
 
+#hw
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+salary_data = pd.read_csv('adults.csv')
+
+salary_data.columns = [
+    'Age', 'Class', 'Sum_Weight', 'Education', 'Edu_Num', 'Marital_status', 
+    'Occupation', 'Relationship', 'Race', 'Gender', 'Capital_gain', 
+    'Capital_loss', 'Hrs/week', 'Native_country', 'Income'
+]
+
+salary_data.replace('?', np.nan, inplace=True)
+salary_data.dropna(how='any', inplace=True)
+
+salary_data.drop(['Sum_Weight', 'Edu_Num', 'Marital_status', 'Capital_gain', 'Capital_loss', 'Native_country', 'Hrs/week'], axis=1, inplace=True)
+
+salary_data['Gender'] = salary_data['Gender'].str.strip().map({'Male': 0, 'Female': 1})
+salary_data['Income'] = salary_data['Income'].str.strip().map({'<=50K': 0, '>50K': 1})
+
+salary_data.groupby('Education')['Income'].mean().plot(kind='bar', figsize=(10, 5))
+
+plt.title('Proportion of High Income (>50K) by Education Level')
+plt.ylabel('Proportion earning >50K')
+plt.xlabel('Education Level')
+plt.xticks(rotation=45, ha='right')
+
+plt.show()
